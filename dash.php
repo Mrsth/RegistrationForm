47bdfba9cd9665a75df642f8ba159cf309dd8687
Mr Sth <36784787+Mrsth@users.noreply.github.com>
2022-07-09 17:21:03 +0545

    Prevent backtrack completed

--- dash.php
+++ dash.php
@@ -1,18 +1,24 @@
 <?php
 session_start();
-?>
-<h1>
-    Welcome to dashboard
-    <?php
-    // to change a session variable, just overwrite it
-    print_r($_SESSION['email']);
-    ?>
-</h1>
 
-<?php
-// remove all session variables
-session_unset();
+if ($_SESSION['flag'] == 0) {
+    echo $_SESSION['flag'];
+    header("Location: index.php");
+}
+echo $_SESSION['flag'];
 
-// destroy the session
-session_destroy();
+if (array_key_exists('button1', $_POST)) {
+    button1();
+}
+function button1()
+{
+    $_SESSION['flag'] = 0;
+    header("Location: index.php");
+}
 ?>
+
+<?php "Welcome to dashboard" . $_SESSION['email'] ?>
+
+<form method="post">
+    <input type="submit" name="button1" class="button" value="Logout" />
+</form>
